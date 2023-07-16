440fb6ac71e2a2efc3670fcf1bca72675d0e62b1
KirpiKafa <ridvant756@gmail.com>
2023-07-16 21:33:52 +0300

    tema yuklendi-sql tablolar olusturuldu

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DbMvc.Models.Entity;
+
+namespace DbMvc.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        CVEntities db = new CVEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.Tbl_About.ToList();
+            return View(degerler);
+        }
+    }
+}
